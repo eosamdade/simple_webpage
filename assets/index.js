22e385e747c8b6aef3e68386c6bdc3ff0b641002
eosamdade <eosamdade@gmail.com>
2022-10-07 13:13:16 -0500

    completed photos of hero section, sign-up page js error and css

--- assets/index.js
+++ assets/index.js
@@ -29,39 +29,76 @@ signUpBtn.addEventListener('click', () => {
 
 window.addEventListener('click', (e) => {
     if(e.target === modal) {
-        modal.style.display = 'none'
+        modal.style.display = 'none';
     }
 });
 
 // form validation
-const form = documnet.getElementById("form");
-const name = documnet.getElementById("name");
-const email = documnet.getElementById("email");
-const password = documnet.getElementById("password");
-const password_confirm = documnet.getElementById("password-confirm");
+const form = document.getElementById("form");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirm = document.getElementById("password-confirm");
 
 // show error
 function showError (input,message) {
     const formValidation = input.parentElement;
-    formValidation.className = 'form-validation error'
+    formValidation.className = 'form-validation error';
 
     const errorMessage = formValidation.querySelector('p');
     errorMessage.innerText = message;
 }
 
-// Event Listeners 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+// show Valid message
+function showValid(input) {
+    const formValidation = input.parentElement;
+    formValidation.className = 'form-validation valid';
+}
 
-    checkRequired([name, email, password, password_confirm])
-})
+// Get fieldname
+function getFieldName(input){
+    return input.name.charAt(0).toUpperCase() + input.name.slice(1);
+}
+
+
 
 // Check required fields
 function checkRequired(inputArr) {
     inputArr.forEach(function(input) {
-        if(inputArr.value.trim() === '') {
-            showError(input, 'Error')
+        if(input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`)
+        }
+        else{
+            showValid(input)
         }
     })
 }
 
+//Check Input length
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    }else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    }else{
+        showValid(input);
+    }
+}
+
+// Check password match
+function passwordMatch(input1, input2) {
+    if(input1.value !== input2.value) {
+        showError(input2, 'Password do not match')
+    }
+}
+
+// Event Listeners 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //preventdefault is not work its suppose to prevent the form from closing
+    checkRequired([name, email, password, passwordConfirm]);
+    checkLength(name,3 ,30);
+    checkLength(password,8 ,25);
+    checkLength(passwordConfirm,8 ,25);
+    passwordMatch(password, passwordConfirm);
+})
