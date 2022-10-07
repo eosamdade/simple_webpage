b4f62280fedf43cef39cca5647ad139ecdd11d15
eosamdade <eosamdade@gmail.com>
2022-10-06 20:16:50 -0500

    completed css for the form, no errors

--- assets/index.js
+++ assets/index.js
@@ -7,3 +7,28 @@ menu.addEventListener('click',function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active')
 })
+
+// modal items
+const modal = document.getElementById("email-modal")
+const openBtn = document.querySelector(".main__btn")
+const closeBtn = document.querySelector(".close-btn")
+const signUpBtn = document.querySelector(".navbar__btn")
+
+//click event
+openBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none'
+});
+
+signUpBtn.addEventListener('click', () => {
+    modal.style.display = 'block'
+});
+
+window.addEventListener('click', (e) => {
+    if(e.target === modal) {
+        modal.style.display = 'none'
+    }
+});
